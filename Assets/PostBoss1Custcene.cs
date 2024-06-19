baea7c1f55a40704ddee25a64426475b4f094da5
UnusualSether <lucaspelzel@gmail.com>
2024-06-19 10:58:56 -0300

    FinalCustcenes Update 1
    +Finished Cutscene 1 and 2 (just need to remove fluff text)
    (Remaining , level 2 start, boss 2 start & ending cutscene)

---
+++ Assets/PostBoss1Custcene.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.SceneManagement;
+
+public class PostBoss1Custcene : MonoBehaviour
+{
+    public string NPCName;
+    [SerializeField] AssetLabelReference portraitsReference;
+
+    [SerializeField]
+    private NPCDialogue npcDialogue;
+
+
+
+    //public List<DialogueLine> lines;
+    public List<DialogueLine> dialogueSet;
+
+    private GameObject marker;
+
+
+    private void Awake()
+    {
+        //marker = GetComponentInChildren<TextMeshPro>().gameObject;
+        //marker.SetActive(false);
+
+
+    }
+
+
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //marker.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //marker.SetActive(false);
+        }
+    }
+
+    public void Dialogue()
+    {
+        Debug.Log(dialogueSet[0].speaker);
+        if (npcDialogue.dialogueStarted == false)
+        {
+            npcDialogue.InitializeDialogue(dialogueSet);
+        }
+        else
+        {
+            npcDialogue.NextText();
+            EndCutscene2();
+
+
+        }
+
+
+    }
+
+    public void EndCutscene2()
+    {
+        if (npcDialogue.activeLine == null)
+        {
+            SceneManager.LoadScene("Level2");
+        }
+
+
+
+    }
+}
---
+++ Assets/PostLevel1Dialogue.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PostLevel1Dialogue : MonoBehaviour
+{
+    [SerializeField]
+    private PostLevel1Custcene npc;
+
+    [SerializeField]
+    private Player player;
+
+
+
+    private void Awake()
+    {
+        player = new Player();
+        npc.Invoke("Dialogue", 1f);
+    }
+
+    private void OnEnable()
+    {
+        player.main.Quack.Enable();
+        player.main.Quack.performed += NextDialogue;
+    }
+
+    private void NextDialogue(InputAction.CallbackContext obj)
+    {
+        Debug.Log("lol");
+        npc.Dialogue();
+    }
+}
