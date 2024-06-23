0a46ad53331f781331fcfec6679940e8c4d39ed2
UnusualSether <lucaspelzel@gmail.com>
2024-06-23 19:03:41 -0300

    Cutscenes with Portraits update
    +cutscene with portraits (excluding Rei Queijao, waiting for them to be pushed)

--- Assets/Code/Dialogue/DialogueSet.cs
+++ Assets/Code/Dialogue/DialogueSet.cs
@@ -9,7 +9,30 @@ public enum SpeakerPortrait {
     placeholder1,
     SelectedPhoto1,
     SelectedPhoto2,
-    SelectedPhoto3
+    SelectedPhoto3,
+    pato_bravo,
+    pato_transtornado,
+    pato_questionando,
+    pato_derrotado,
+    pato_envergonhado,
+    pato_neutro,
+    pato_neutro_olhando_jogador,
+    pato_puto,
+    pato_superior,
+    pato_tenso,
+    pardais_irritados,
+    pardal_assustado,
+    pardal_irritado,
+    pardal_neutro,
+    pardal_risada,
+    pombo_neutro,
+    pombo_vangloriando,
+    cisne_agradecida,
+    cisne_apaixonada,
+    cisne_irritada,
+    cisne_medo,
+    cisne_neutra,
+    narrator
 }
 
 
